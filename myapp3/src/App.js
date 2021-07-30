76d3e18e0709ee84d8bec0eb022073d4a97ece8b
mahesh0455 <mahesh.p.pawar045@gmail.com>
2021-07-30 12:17:11 +0530

    Creating userdefined tags

--- myapp3/src/App.js
+++ myapp3/src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div>
-      <h1>
-        Hello World
-      </h1>
+      <HelloWorld></HelloWorld>
+      <HelloWorld></HelloWorld>
+      <HelloWorld></HelloWorld>
+      <HelloUniverse></HelloUniverse>
+      <HelloUniverse></HelloUniverse>
+
+      
+
+
     </div>
   );
 }
 
-export default App;
+//export default App;
+
+// User defined tag::component......<helloWorld/>
+//use pascal case for userdefined tags
+function HelloWorld() {
+  return <h1>Hello World</h1>
+}
+
+function HelloUniverse() {
+  return <h1>Hello Universe</h1>
+}
